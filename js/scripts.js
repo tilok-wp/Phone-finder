262c3dbda47b581dc76b6434849966ce64dd3e9b
Tilok Paul <tilokpaul.web@gmail.com>
2022-03-01 00:13:55 +0600

    Get input text option create

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const getSearchText = () => {
+    const searchText = document.getElementById('search-text')
+    let searchValue = searchText.value
+    console.log(searchValue)
+    searchValue = ''
+}
